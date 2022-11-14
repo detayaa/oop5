2d902c956b7e8ff4a69f2bef48939576f0c97fc6
detayaa <drugochenko.taya@mail.ru>
2022-11-14 23:27:37 +0700

    1

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,7 @@ public class Main {
         Product apple = new Product("Яблоки",5, 13);
         Product lime = new Product("Лайм",3, 7);
 
-        Set<Product> list = new HashSet<>();
-        list.add(banana);
-        list.add(lime);
-        list.add(banana);
-        list.add(apple);
+
 
 
 
--- src/ProductList.java
+++ src/ProductList.java
@@ -3,6 +3,9 @@ import java.util.Objects;
 import java.util.Set;
 
 public class ProductList<T extends Product> {
+
+    String DOUBLE_PRODUCTS = "Нельзя добавить продукт второй раз";
+
     Set<Product> list = new HashSet<>();
 
     public void addList() {
